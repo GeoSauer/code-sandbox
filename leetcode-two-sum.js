64c9ca87a0e0415f67862c5cd46e0f9dd267a751
GeoSauer <geoffrey.sauer89@gmail.com>
2023-07-31 13:05:48 -0600

    adds completed two-sum leetcode problem

---
+++ leetcode-two-sum.js
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/two-sum/description/
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+const nums = [3, 2, 4];
+const target = 6;
+
+const twoSum = (nums, target) => {
+  //instantiating an empty Map object
+  const numsMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    //defining the other number to look for
+    const difference = target - nums[i];
+
+    //checking to see if the Map already has a key matching difference
+    if (numsMap.has(difference)) {
+      //if it does, we return an array with the current index and the index of the difference variable
+      return [i, numsMap.get(difference)];
+    }
+    //else a key of the current number with a value of the number's index is stored in the map
+    numsMap.set(nums[i], i);
+  }
+};
+
+console.log(twoSum(nums, target));
