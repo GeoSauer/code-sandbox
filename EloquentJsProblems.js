f13bbf553391795429eb578836c4a068d687d9af
GeoSauer <geoffrey.sauer89@gmail.com>
2023-08-21 11:29:50 -0600

    adds EloquentJsProblems and first solution

---
+++ EloquentJsProblems.js
@@ -0,0 +1,16 @@
+//? Write a loop that makes seven calls to console.log to output the following triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+//* const triangleBuilder = () => {
+//   for (let triangle = ""; triangle.length < 8; triangle += "#") {
+//     console.log(triangle);
+//   }
+// };
+
+// triangleBuilder();
