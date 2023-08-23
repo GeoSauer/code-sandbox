639c78516dc4fa0497b92e8311b6e709de3c2ecc
GeoSauer <geoffrey.sauer89@gmail.com>
2023-08-23 13:00:14 -0600

    adds Bean Counting and solutions

--- EloquentJsProblems.js
+++ EloquentJsProblems.js
@@ -143,3 +143,37 @@
 // console.log(isEven(50));
 // console.log(isEven(75));
 // console.log(isEven(-1));
+
+//? 1. Bean counting - function that takes in a string and returns the number of 'B's in the string
+//? 2. next write a function that counts the characters specified by a second argument
+//? 3. re-write first function to include second's functionality
+
+//* Solution 1
+// const countBs = (string) => {
+//   let capitalBs = 0;
+//   for (let i = 0; i < string.length; i++) {
+//     if (string[i] === "B") {
+//       capitalBs++;
+//     }
+//   }
+//   return capitalBs;
+// };
+// console.log(countBs("There are 2 B in this Bentence"));
+
+//* Solution 2
+// const countChar = (string, char) => {
+//   let chars = 0;
+//   for (let i = 0; i < string.length; i++) {
+//     if (string[i] === char) {
+//       chars++;
+//     }
+//   }
+//   return chars;
+// };
+// console.log(countChar("There are 2 B in this Bentence", "e"));
+
+//* Solution 3
+// const countBChars = (string) => {
+//   return countChar(string, "B");
+// };
+// console.log(countBChars("There are 2 B in this Bentence"));
